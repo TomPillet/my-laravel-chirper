16642a6a65a1e5fc0419ea9b8571e1513fc8eade
Tom <tom963741@gmail.com>
2023-07-13 05:56:48 +0200

    feat: enable chirp editing

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -36,6 +36,7 @@ class ChirpController extends Controller
         $validated = $request->validate([
             'message' => 'required|string|max:255',
         ]);
+
         $request->user()->chirps()->create($validated);
 
         return redirect(route('chirps.index'));
@@ -60,9 +61,16 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp);
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
